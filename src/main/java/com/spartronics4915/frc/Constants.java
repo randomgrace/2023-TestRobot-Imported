223c9eb5f445eb5cd515ea1b208ff8c9136f0eb0
Randy Groves <randy.groves@boeing.com>
2023-02-06 18:07:43 -0800

    Use Limelight

--- src/main/java/com/spartronics4915/frc/Constants.java
+++ src/main/java/com/spartronics4915/frc/Constants.java
@@ -60,7 +60,7 @@ public final class Constants {
                                 0)); // Cam mounted facing forward, half a meter forward of center, half a meter up
         // from center.
         // Limelight
-		// static final String cameraName = "OV5647";
-        static final String cameraName = "HD_Pro_Webcam_C920";
+		static final String cameraName = "OV5647";
+        // static final String cameraName = "HD_Pro_Webcam_C920";
     }
 }
